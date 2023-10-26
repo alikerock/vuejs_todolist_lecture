a85ccece9432654678b2a47426c6c1a0984fc7c4
rock's ezweb <alikerock@gmail.com>
2023-10-26 12:10:59 +0900

    list output

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script>
+import Todo from './components/Todo.vue';
+
   export default {
+    components:{
+      Todo
+    },
     data(){
       return {
         todoText : '',
@@ -16,9 +21,10 @@
   <div class="container">
     <h1 class="text-center">Todo App</h1>
     <div class="mb-3">
-  <label for="todo" class="form-label">Todo Input</label>
-  <input type="text" class="form-control" id="todo" placeholder="할일을 입력하세요">
-</div>
+      <label for="todo" class="form-label">Todo Input</label>
+      <input type="text" class="form-control" id="todo" placeholder="할일을 입력하세요">
+    </div>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo"/>
   </div>
 </template>
 
